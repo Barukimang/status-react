0360a519fc5afca8988dba970362de69c1debc65
Samuel Hawksby-Robinson <samuel@samyoul.com>
2021-03-19 13:58:46 +0000

    Added transformations ns and funcs

--- src/status_im/anon_metrics/interceptors.cljs
+++ src/status_im/anon_metrics/interceptors.cljs
@@ -1,34 +1,27 @@
 (ns status-im.anon-metrics.interceptors
   (:require [status-im.ethereum.json-rpc :as json-rpc]
             [taoensso.timbre :as log]
-            [re-frame.core :as re-frame]
-            [status-im.utils.fx :as fx]
-            [re-frame.interceptor :refer [->interceptor get-coeffect]]))
+            [re-frame.interceptor :refer [->interceptor get-coeffect]]
+            [status-im.utils.platform :as platform]
+            [status-im.utils.build :as build]
+            [status-im.anon-metrics.transformers :as txf]))
 
 (defn catch-events-fn [context]
-    (let [[event-type view-id params] (get-in context [:coeffects :event])]
-      (let [filtered-params (filter (fn [[k v]] (= :screen k)) params)]
-      [
-       (log/info :catch-event-fn event-type view-id params filtered-params)
-       (json-rpc/call {
-                       :method     "appmetrics_validateAppMetrics"
-                       :params     [[{:event event-type
-                                      :val {
-                                            :view_id view-id
-                                            :params {:screen filtered-params}
-                                            }
-                                      :app_version "" ;; TODO(samyoul) work out how to get this
-                                      :os ""}]] ;; TODO(samyoul) work out how to get this
-                       :on-success log/debug "appmetrics_validateAppMetrics successful"
-                       :on-failure log/debug "appmetrics_validateAppMetrics failure"})
-       ])))
+  (log/info :catch-event-fn (get-in context [:coeffects :event]))
+  (when-let [transformed-payload (txf/transform context)]
+       (json-rpc/call {:method "appmetrics_validateAppMetrics"
+                       :params [[{:event (get-in context [:coeffects :event :event-type])
+                                  :val transformed-payload
+                                  :app_version build/version
+                                  :os platform/os}]]
+                       :on-failure log/error})))
+
+(defn catch-events-before [context]
+  (log/info "catch-events/interceptor fired")
+  (catch-events-fn context)
+  context)
 
 (def catch-events
   (->interceptor
     :id     :catch-events
-    :before (fn catch-events-before [context]
-              [
-                (log/info "catch-events/interceptor fired")
-                (catch-events-fn context)
-               ]
-              context)))
+    :before catch-events))
